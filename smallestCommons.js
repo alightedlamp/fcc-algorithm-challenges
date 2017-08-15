a061b65eac2fab2a7b6236549d0c04988b7ab4ff
Philip Harrell <alightedlamp@gmail.com>
2017-08-15 13:55:56 -0500

    find element

--- smallestCommons.js
+++ smallestCommons.js
@@ -1,9 +1,6 @@
-// Find the smallest common multiple of both 1 and 3 that is evenly divisible by all numbers between arr[0] and arr[1].
-// should be for the RANGE, so 1, 5 should check for the lowest of 1, 2, 3, 4, 5
 function smallestCommons(arr) {
-  Array.prototype.range = function(a, b) {
-    const start = a;
-    const end = b;
+  // Add a method on Array prototype just for fun
+  Array.prototype.range = function(start, end) {
     let arr = [];
 
     for (let i = start; i < end + 1; i++) {
@@ -12,8 +9,10 @@ function smallestCommons(arr) {
 
     return arr;
   }
+  // Sort it, range it, reverse it
   arr = arr.sort((a, b) => a - b).range(arr[0], arr[1]).reverse();
 
+  // Set up variables for loops
   let quot = 0;
   let idx = 1;
   let n;
